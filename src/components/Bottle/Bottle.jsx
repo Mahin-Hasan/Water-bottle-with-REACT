75dbdd5d43612c037ff165d6184a1866d942fe68
Mahin Hasan <mahinmtrs@gmail.com>
2023-10-28 22:08:05 +0600

    added cart section

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,11 +1,13 @@
-const Bottle = ({bottle}) => {
+import './Bottle.css'
+const Bottle = ({ bottle ,handleAddToCart}) => {
     // console.log(bottle);
-    const {name,img,price}= bottle
+    const { name, img, price } = bottle
     return (
         <div className="bottle">
             <h3>Bottle: {name}</h3>
             <img src={img} />
             <p>Price: ${price}</p>
+            <button onClick={handleAddToCart}>Purchase</button>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,25 +1,38 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
+import './Bottles.css'
 
 const Bottles = () => {
-    const [bottles, setBottles] = useState([])
-    useEffect(()=>{
+    const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
         fetch('bottles.json')
-        .then(res=>res.json())
-        .then(data=>setBottles(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBottles(data))
+    }, [])
+
+    const handleAddToCart = bottle => {
+        // console.log('tbh',bottle);
+        const newCart = [...cart,bottle];
+        setCart(newCart);
+    }
+
 
     return (
         <div>
-            <h2>Bottle Total: {bottles.length}</h2>
-            {
-                bottles.map(bottle=><Bottle
-                key={bottle.id}
-                bottle={bottle}
-                ></Bottle>)
+            <h2>Bottle Available: {bottles.length}</h2>
+            <h4>Cart: {cart.length}</h4>
+            <div className="bottle-container">
+                {
+                    bottles.map(bottle => <Bottle
+                        key={bottle.id}
+                        bottle={bottle}
+                        handleAddToCart={() => handleAddToCart(bottle)}
+                    ></Bottle>)
 
-            }
+                }
+            </div>
         </div>
     );
 };
